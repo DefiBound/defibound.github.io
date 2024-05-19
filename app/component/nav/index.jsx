e8a02631542b794ccd6e51ef5d11c01c61d6faa8
yyyhakuna <yjx020714@icloud.com>
2024-05-19 16:41:37 +0800

    the homepage

---
+++ app/component/nav/index.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import style from './index.module.css'
+const index = () => {
+    return (
+        <div className={style.container}>
+            <span>DEFIBOUNCE</span>
+            <button class={style.cssbuttons_io_button}>
+                Get started
+                <div class={style.icon}>
+                    <svg
+                        height="24"
+                        width="24"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path d="M0 0h24v24H0z" fill="none"></path>
+                        <path
+                            d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                            fill="currentColor"
+                        ></path>
+                    </svg>
+                </div>
+            </button>
+            <button class={style.cssbuttons_io_button}>
+                Get started
+                <div class={style.icon}>
+                    <svg
+                        height="24"
+                        width="24"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path d="M0 0h24v24H0z" fill="none"></path>
+                        <path
+                            d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                            fill="currentColor"
+                        ></path>
+                    </svg>
+                </div>
+            </button>
+            <button class={style.cssbuttons_io_button}>
+                Get started
+                <div class={style.icon}>
+                    <svg
+                        height="24"
+                        width="24"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path d="M0 0h24v24H0z" fill="none"></path>
+                        <path
+                            d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                            fill="currentColor"
+                        ></path>
+                    </svg>
+                </div>
+            </button>
+        </div>
+    )
+}
+
+export default index
